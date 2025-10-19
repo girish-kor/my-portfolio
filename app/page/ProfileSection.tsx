e979c0fefde9275020f90f6c076a704e68b3aec0
girish-kor <2021ci83d@sigce.edu.in>
2025-10-19 17:48:49 +0530

    fix: update font size for Software Developer title in ProfileSection for improved visibility

--- app/page/ProfileSection.tsx
+++ app/page/ProfileSection.tsx
@@ -44,7 +44,7 @@ export function ProfileSection() {
             />
           </svg>
           <h2 className="text-2xl font-bold">Girish Kor</h2>
-          <p className="text-sm">Software Developer</p>
+          <p className="text-xs">Software Developer</p>
         </div>
       </div>
       {/* DIVISION 2: Description & Social Links */}
