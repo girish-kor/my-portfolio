7e60a97bff331d1175c85c69b6dccfd93751f5b1
girish-kor <2021ci83d@sigce.edu.in>
2025-10-19 19:25:23 +0530

    refactor: remove unused repository fetching logic and streamline CountSection

--- app/page/CountSection.tsx
+++ app/page/CountSection.tsx
@@ -1,45 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
+import { ThemeSwitch } from '~/components/theme-switch';
 
 export function CountSection() {
-  const username = 'girish-kor';
-  const [repoCount, setRepoCount] = useState<number | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function fetchRepoCount() {
-      try {
-        const res = await fetch(`https://api.github.com/users/${username}`);
-        if (!res.ok) throw new Error('Failed to fetch GitHub user data');
-        const data = await res.json();
-        setRepoCount(data.public_repos); // GitHub API gives total public repos
-      } catch (err: any) {
-        setError(err.message || 'Error fetching data');
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchRepoCount();
-  }, []);
-
   return (
-    <div className="text-center opacity-80">
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {repoCount !== null && (
-        <div className=" flex flex-col items-center">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <p className="cursor-help text-4xl">
-                <strong>{repoCount}</strong>
-              </p>
-            </TooltipTrigger>
-            <TooltipContent sideOffset={4}>Total public repositories on GitHub</TooltipContent>
-          </Tooltip>
-        </div>
-      )}
+    <div className="text-center ">
+      <ThemeSwitch className="rotate-90 border-gray-300 border-1 dark:border-white" />
     </div>
   );
 }
